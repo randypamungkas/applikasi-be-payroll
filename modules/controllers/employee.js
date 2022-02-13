a6edc1fe81b0ee3acba690f3710f1c50e7636177
FahrasNH <fahras.fnh@gmail.com>
2022-02-13 11:30:37 +0700

    Create pdf

--- modules/controllers/employee.js
+++ modules/controllers/employee.js
@@ -1,22 +1,27 @@
-const employeeRepository = require("../repositories/employee");
-const {
-  failed,
-  success,
-  successAndCount,
-} = require("../../helper/response");
+const employeeRepository = require('../repositories/employee')
+const { failed, success, successAndCount } = require('../../helper/response')
+const moment = require('moment')
+const pdf = require('pdf-creator-node')
+const fs = require('fs')
+
+// Read HTML Template
+const html = fs.readFileSync(`${__dirname}/../templates/employee.html`, 'utf8')
 
 module.exports = {
   employeeGetAll: async (req, res) => {
     try {
-      const {
-        rows,
-        count,
-        pages
-      } = await employeeRepository.getAllEmployee(req, res);
+      const { rows, count, pages } = await employeeRepository.getAllEmployee(
+        req,
+        res,
+      )
 
       res.status(200)
       successAndCount(
-        res, rows, count, pages, rows.length < 1 ? "Data tidak ditemukan" : "Sukses"
+        res,
+        rows,
+        count,
+        pages,
+        rows.length < 1 ? 'Data tidak ditemukan' : 'Sukses',
       )
     } catch (err) {
       res.status(400)
@@ -26,10 +31,10 @@ module.exports = {
 
   getDetailEmployee: async (req, res) => {
     try {
-      const response = await employeeRepository.getDetailEmployee(req, res);
+      const response = await employeeRepository.getDetailEmployee(req, res)
 
       res.status(200)
-      success(res, response || [], "Sukses")
+      success(res, response || [], 'Sukses')
     } catch (err) {
       res.status(400)
       failed(res, [], err.message)
@@ -38,10 +43,10 @@ module.exports = {
 
   createEmployee: async (req, res) => {
     try {
-      const response = await employeeRepository.createEmployee(req, res);
+      const response = await employeeRepository.createEmployee(req, res)
 
       res.status(200)
-      success(res, response || [], "Karyawan berhasil ditambahkan")
+      success(res, response || [], 'Karyawan berhasil ditambahkan')
     } catch (err) {
       res.status(400)
       failed(res, [], err.message)
@@ -50,10 +55,10 @@ module.exports = {
 
   updateEmployee: async (req, res) => {
     try {
-      const response = await employeeRepository.updateEmployee(req, res);
+      const response = await employeeRepository.updateEmployee(req, res)
 
       res.status(200)
-      success(res, response || [], "Karyawan berhasil diperbarui")
+      success(res, response || [], 'Karyawan berhasil diperbarui')
     } catch (err) {
       res.status(400)
       failed(res, [], err.message)
@@ -62,13 +67,58 @@ module.exports = {
 
   deleteEmployee: async (req, res) => {
     try {
-      const response = await employeeRepository.deleteEmployee(req, res);
+      const response = await employeeRepository.deleteEmployee(req, res)
 
       res.status(200)
-      success(res, response || [], "Data berhasil dihapus")
+      success(res, response || [], 'Data berhasil dihapus')
     } catch (err) {
       res.status(400)
       failed(res, [], err.message)
     }
-  }
-};
+  },
+
+  printPdfPaySlipsEmployee: async (req, res) => {
+    try {
+      const response = await employeeRepository.getDetailEmployee(req, res)
+
+      const options = {
+        format: 'A5',
+        orientation: 'landscape',
+        border: '10mm',
+      }
+
+      const document = {
+        html: html,
+        header: {
+          height: '45mm',
+          contents:
+            '<div style="text-align: center;">Author: Shyam Hajare</div>',
+        },
+        data: {
+          employee: {
+            ...response,
+            salary: Intl.NumberFormat('en-US').format(response.salary),
+            createdAt: moment(response.createdAt).format('D MMM YYYY'),
+            updatedAt: moment(response.updatedAt).format('D MMM YYYY'),
+            salary_received: Intl.NumberFormat('en-US').format(
+              response.salary_received,
+            ),
+          },
+        },
+        path: './output.pdf',
+      }
+
+      pdf
+        .create(document, options)
+        .then((resDocs) => {
+          res.status(200).sendfile(resDocs.filename)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    } catch (err) {
+      res.status(400)
+      failed(res, [], err.message)
+    }
+  },
+}
